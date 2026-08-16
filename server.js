52b049c649d77b605f07913d64d56feb02f63b52
omikarn97-oss <omikarn97@gmail.com>
2026-08-16 20:07:40 +0545

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const genAI = new GoogleGenerativeAI(
 );
 
 const model = genAI.getGenerativeModel({
-    model: "gemini-2.5-flash"
+    model: "gemini-3.5-flash"
 });
 
 app.get("/", (req, res) => {
